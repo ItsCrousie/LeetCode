faa36f8205c5898b8a6891574910e0e763ef2963
ItsCrousie <itscrousie@gmail.com>
2022-12-20 09:40:07 -0500

    Git Test

---
+++ longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var lengthOfLongestSubstring = function(s) {
+  let subString = ''
+  let longestSubstring = ''
+  for (let i = 0; i < s.length; i += 1) {
+    if (subString.indexOf(s[i]) === -1) {
+      subString += s[i]
+      console.log('substring', subString,'longest substring', longestSubstring, 'input', s)
+    } else {
+      subString = ''
+    }
+    if (subString.length > longestSubstring.length) {
+      longestSubstring = subString
+    }
+  }
+  return longestSubstring.length;
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
+console.log(lengthOfLongestSubstring('bbbbb'))
+// console.log(lengthOfLongestSubstring('pwwkew'))
