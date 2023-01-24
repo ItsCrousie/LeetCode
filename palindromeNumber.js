84837f86b6836d4214fbf35405ce407d4cdaede5
ItsCrousie <itscrousie@gmail.com>
2023-01-24 08:40:51 -0500

    Solved 9.Palindrome Number

---
+++ palindromeNumber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  x += '';
+  xReverse = x.split('').reverse().join('');
+  if (x === xReverse) {
+      return true;
+  } else {
+      return false;
+  }
+};
