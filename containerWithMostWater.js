a76eecb047117bd89617bf55217be358f2cff4bb
ItsCrousie <itscrousie@gmail.com>
2023-01-30 15:43:12 -0500

    solve containerWithMostWater

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -3,33 +3,16 @@
  * @return {number}
  */
 var maxArea = function(height) {
-  let maxLIdx = 0
-  let maxL = height[maxLIdx];
-  let maxRIdx = height.length-1
-  let maxR = height[maxRIdx];
-  let maxArea = 0
-
-  const calcContainer = (l, lIdx, r, rIdx) => {
-    return Math.min(l, r) * (rIdx - lIdx)
+  let area = 0;
+  let left = 0;
+  let right = height.length-1
+  while (left < right) {
+    area = Math.max(area, Math.min(height[left], height[right]) * (right - left))
+    height[left] <= height[right]
+    ? left += 1
+    : right -= 1
   }
-
-  for (let i = 0; i < height.length; i += 1) {
-    for (let j = height.length-1; j > i; j -= 1) {
-      const l = height[i];
-      const r = height[j];
-      const currentArea = calcContainer(l, i, r, j)
-      if (currentArea > maxArea && l > maxL) {
-        maxL = l
-        maxLIdx = i
-      }
-      if (currentArea > maxArea && r > maxR) {
-        maxR = r
-        maxRIdx = j
-      }
-      maxArea = calcContainer(maxL, maxLIdx, maxR, maxRIdx)
-    }
-  }
-  return maxArea
+  return area;
 };
 
-//times out 55/61 cases
+//accepted
